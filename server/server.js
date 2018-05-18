ca53bf11a79423d135390187b9cd11dc3999d97d
Ihor Hrysha <ihor.hrysha@gmail.com>
2018-05-18 19:27:58 +0300

    fix render bug

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const hbs = require('hbs');
 const path = require('path');
 const express = require('express');
 const app = express();
@@ -5,8 +6,9 @@ const app = express();
 const publicPath = path.join(__dirname,'..','public');
 const port = process.env.PORT || 3000;
 
+app.set('view engine', 'hbs');
 app.use(express.static(publicPath));
-app.render('/', (req, res) => res.send('Hello World!'));
+//app.render('/', (req, res) => res.send('Hello World!'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
