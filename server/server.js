c16a90b63bfc3eb1d57a5ed6e230bf738222739e
Ihor Hrysha <ihor.hrysha@gmail.com>
2018-05-18 23:08:48 +0300

    Connection listners

--- server/server.js
+++ server/server.js
@@ -1,15 +1,29 @@
-const hbs = require('hbs');
+//const hbs = require('hbs');
 const path = require('path');
 const express = require('express');
-const app = express();
+const http = require('http');
+const socketio = require('socket.io');
 
-const publicPath = path.join(__dirname,'..','public');
+const publicPath = path.join(__dirname, '..', 'public');
 const port = process.env.PORT || 3000;
 
-app.set('view engine', 'hbs');
+const app = express();
+var server = http.createServer(app);
+var io = socketio(server);
+
+io.on('connection', function (socket) {
+    console.log('a user connected');
+
+    socket.on('disconnect', () => {
+        console.log('Disonnected client');
+    });
+
+});
+
+
+
+//app.set('view engine', 'hbs');
 app.use(express.static(publicPath));
 //app.render('/', (req, res) => res.send('Hello World!'));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-
-console.log(publicPath);
+server.listen(port, () => console.log(`Example app listening on port ${port}!`));
