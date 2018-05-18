d60497c946c47d75dd02fe3f10beb0db227437da
Ihor Hrysha <ihor.hrysha@gmail.com>
2018-05-18 19:09:33 +0300

    init commit

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+
+const publicPath = path.join(__dirname,'..','public');
+const port = process.env.PORT || 3000;
+
+app.use(express.static(publicPath));
+app.render('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+console.log(publicPath);
